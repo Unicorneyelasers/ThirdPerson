d588235bf9145540ede30c93c09874256539ab04
Jacqueline Miner <58049145+Unicorneyelasers@users.noreply.github.com>
2022-02-01 14:10:33 -0800

    Got it jumping!
    Finished end of class demo, works

--- Third Person/Assets/Scenes/Script/PlayerMovement.cs
+++ Third Person/Assets/Scenes/Script/PlayerMovement.cs
@@ -7,25 +7,52 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     private float speed = 6.0f;
     private float rotSpeed = 360.0f;
+   //gravity variables 
+    private float gravity = -9.81f;
+    private float yVelocity = 0.0f;
+    private float yVelocityGrounded = -4.0f;
+    //jump veriables
+    private float jumpHeight = 3.0f;
+    private float jumpTime = 0.5f;
+    private float initialjumpVelocity;
+
    [SerializeField]
     public CharacterController cc;
 
     void Start()
     {
-        
 
-       // cc = GetComponent<CharacterController>();
+        float timetoApex = jumpTime / 2.0f;
+        gravity = (-2 * jumpHeight) / Mathf.Pow(timetoApex, 2);
+
+        initialjumpVelocity = (2 * jumpHeight) / timetoApex;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+       
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        //transform.Translate(movement * Time.deltaTime * speed);
-        
         movement = transform.TransformDirection(movement);
         movement = Vector3.ClampMagnitude(movement, 1.0f);
-        cc.Move(movement * Time.deltaTime * speed);
+        movement *= speed;
+
+        yVelocity += gravity * Time.deltaTime;
+        if (cc.isGrounded && yVelocity < 0.0)
+        {
+            yVelocity = yVelocityGrounded;
+        }
+
+
+        
+        if (Input.GetButtonDown("Jump") && cc.isGrounded)
+        {
+            yVelocity = initialjumpVelocity;
+        }
+
+        movement.y = yVelocity;
+        cc.Move(movement * Time.deltaTime);
 
         Vector3 rotation = new Vector3(0, Input.GetAxis("Mouse X"), 0);
         transform.Rotate(rotation * Time.deltaTime * rotSpeed);
